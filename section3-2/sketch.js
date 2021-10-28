16491a97bdb24e5451eadc39fb3a6cc91a493917
yukakojima712 <yukakojima712@gmail.com>
2021-10-29 00:19:10 +0900

    change3-q1
    心臓の鼓動

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -21,10 +21,10 @@ function draw(){
   vy = constrain(vy + g, -vyMax, vyMax);
 
   // 端の処理パターン (1) 反対側から出てくる
-  // if(x > width){ x = 0; }
-  // else if(x < 0){ x = width; }
-  // if(y > height){ y = 0; }
-  // if(y < 0){ y = height; }
+  //if(x > width){ x = 0; }
+  //else if(x < 0){ x = width; }
+  //if(y > height){ y = 0; }
+  //if(y < 0){ y = height; }
 
 　// 端の処理パターン (2) 跳ね返る
   if(x < 0 || x > width){ vx = -1 * vx; }
