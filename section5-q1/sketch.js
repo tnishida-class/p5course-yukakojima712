5f3226f7dac8c305fc55aec8bbc32b939bde8703
yukakojima712 <yukakojima712@gmail.com>
2021-11-14 23:57:17 +0900

    change5-q1
    吹き出し

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 100, 100, 0, 255, 0);
 }
 
-function balloon(t){
+function balloon(t, cx, cy, r, g, b){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  noStroke();
+  fill(r, g, b);
+  rect(cx, cy, w + p * 2, h + p * 2);
+  triangle(cx+w-10, cy+(h+p*2), cx+(w+p*2), cy+(h+p*2), cx+w+p, cy+(h+p*5));
   fill(255);
-  text(t, p, h + p);
+  text(t, cx + p, cy + h);
 }
